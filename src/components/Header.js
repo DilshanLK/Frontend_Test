f97fc7ee6059f8b8f43def66c7a0fc44f37dad1a
dilshanpanduwawala <dilshanpanduwawala@gmail.com>
2021-07-01 13:37:45 +0530

    Refactor Hero Section

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,71 +8,75 @@ import TwitterIcon from '../assets/images/icon-4.png'
 import VIcon from '../assets/images/icon-5.png'
 import Search from '../assets/images/magnifying-glass.svg'
 import HeaderBackground from '../assets/images/banner.png'
+import './Header.css'
 
 const Header = () => {
   return (
     <div
-      class='bg-center'
-      style={{ backgroundImage: `url(${HeaderBackground})` }}>
-      <div class='container mx-auto z-10 relative'>
-        <div class='md:flex md:items-center md:text-left md:justify-between py-8'>
-          <div class='flex justify-between w-4/12 text-base gray-700 text-white'>
-            <div>Call US</div>
-            <div class='text-orange-200'>029292162</div>
-            <div>Career</div>
+      className='bg-center'
+      style={{
+        backgroundImage: `url(${HeaderBackground})`,
+        backgroundRepeat: 'no-repeat',
+      }}>
+      <div className='container mx-auto z-10 relative pt-10'>
+        <div className='md:flex md:items-center md:text-left md:justify-between'>
+          <div className='flex items-center w-4/12 text-base gray-700 text-white'>
+            <div className='header_menu'>Call US</div>
+            <div className='header_menu_mobile'>029292162</div>
+            <div className='header_menu'>Career</div>
             <div>Support</div>
           </div>
-          <div class='w-5/12 mx-auto md:m-0 flex justify-center'>
-            <img src={Logo} class='h-10' />
+          <div className='w-4/12 mx-auto md:m-0 flex justify-center'>
+            <img src={Logo} className='header_logo' />
           </div>
-          <div class='flex mx-auto md:m-0 justify-between w-3/12'>
+          <div className='flex justify-end w-4/12'>
             <div>
-              <img src={LinkedinIcon} class='h-4' />
+              <img src={LinkedinIcon} className='social_media_icons' />
             </div>
             <div>
-              <img src={FacebookIcon} class='h-4' />
+              <img src={FacebookIcon} className='social_media_icons' />
             </div>
             <div>
-              <img src={GoogleIcon} class='h-4' />
+              <img src={GoogleIcon} className='social_media_icons' />
             </div>
             <div>
-              <img src={TwitterIcon} class='h-4' />
+              <img src={TwitterIcon} className='social_media_icons' />
             </div>
             <div>
-              <img src={VIcon} class='h-4' />
+              <img src={VIcon} className='social_media_icons' />
             </div>
             <div>
-              <img style={{ color: '#f59019' }} src={Search} class='h-4' />
+              <img src={Search} className='search_icon' />
             </div>
           </div>
         </div>
-        <div class='flex justify-between bg-blue-200 py-4'>
-          <div class='flex items-center w-6/12 justify-between pl-8 text-white'>
-            <div>Home</div>
-            <div>Services</div>
-            <div>Portfolio</div>
-            <div>News</div>
-            <div>About</div>
-            <div>Contact</div>
+        <div className='flex justify-between bg-blue-200 nav_menu_box'>
+          <div className='flex nav_menu items-center w-6/12 pl-8 text-white'>
+            <div className='nav_menu_button'>Home</div>
+            <div className='nav_menu_button'>Services</div>
+            <div className='nav_menu_button'>Portfolio</div>
+            <div className='nav_menu_button'>News</div>
+            <div className='nav_menu_button'>About</div>
+            <div className='nav_menu_button'>Contact</div>
           </div>
-          <div class='w-6/12 flex  justify-end pr-4'>
-            <div class='bg-orange-200 rounded-lg text-white px-8 py-2'>
+          <div className='w-6/12 flex justify-end items-center pr-4'>
+            <div className='bg-orange-200 flex justify-center rounded-lg text-white apply_button'>
               <button>Apply Now</button>
             </div>
           </div>
         </div>
-        <div class='flex justify-center pt-20'>
-          <div class='text-center text-white'>
-            <div class='text-base'>Protfolio</div>
-            <div class='text-5xl pt-4'>About US</div>
-            <div class='w-48 h-1 bg-white mt-4'></div>
+        <div className='flex justify-center'>
+          <div className='text-center text-white'>
+            <div className='portfolio_text'>Protfolio</div>
+            <div className='about_us_text'>About Us</div>
+            <div className='divider_line'></div>
           </div>
         </div>
-        <div class='flex justify-center pt-20 pb-10'>
-          <div class='flex'>
-            <div class='text-white'>Home</div>
-            <div class='px-2 text-white'>-</div>
-            <div class='text-orange-200'>Portfolio</div>
+        <div className='flex justify-center tab_menu pb-10'>
+          <div className='flex'>
+            <div className='text-white tab_menu_button'>Home</div>
+            <div className='px-2 text-white tab_menu_button'>-</div>
+            <div className='text-orange-200 tab_menu_button'>Portfolio</div>
           </div>
         </div>
       </div>
